81e861d1e5ecd99116ac3d8ebd3d63958efb3ea6
Ezra Bartlett <ezra_bartlett@homedepot.com>
2022-03-08 15:19:06 -0600

    api

--- js/myapi.js
+++ js/myapi.js
@@ -3,7 +3,7 @@ const baseURL = `https://young-lake-21798.herokuapp.com/`;
 const Http = new XMLHttpRequest();
 
 const doCurl = async (action) => {
-    Http.open("POST", baseURL+action);
+    Http.open("GET", baseURL+action);
     Http.setRequestHeader('Access-Control-Allow-Headers', '*');
     Http.setRequestHeader('Content-type', 'siteAnalytics');
     Http.setRequestHeader('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS');
